7f31588066b61f0ac88fad5c0b345de84d79e2a1
ajyalna-admin <ajyalnaalwaidah@gmail.com>
2026-07-31 00:12:47 +0300

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,9 +35,10 @@ export default function RootLayout({
         className={`${cairo.variable} ${tajawal.variable} font-body antialiased bg-cream text-navy`}
       >
         <div className="bg-orbs" aria-hidden="true">
-          <span className="h-[420px] w-[420px] bg-sky/50 -top-32 -right-32" />
-          <span className="h-[380px] w-[380px] bg-navy/20 top-[35%] -left-40" />
-          <span className="h-[340px] w-[340px] bg-sky-deep/30 bottom-0 right-1/4" />
+          <span className="h-[460px] w-[460px] bg-sky/60 -top-32 -right-32" />
+          <span className="h-[420px] w-[420px] bg-sky-deep/35 top-[38%] -left-44" />
+          <span className="h-[380px] w-[380px] bg-navy/15 bottom-[-80px] right-1/4" />
+          <span className="h-[300px] w-[300px] bg-sky/45 top-[70%] left-1/3" />
         </div>
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@ import { Navbar } from "@/components/Navbar";
 import { Hero } from "@/components/Hero";
 import { About } from "@/components/About";
 import { Vision } from "@/components/Vision";
-import { Structure } from "@/components/Structure";
 import { Programs } from "@/components/Programs";
 import { Resources } from "@/components/Resources";
-import { Excellence } from "@/components/Excellence";
 import { Impact } from "@/components/Impact";
 import { Team } from "@/components/Team";
 import { Contact } from "@/components/Contact";
@@ -20,10 +18,8 @@ export default function Home() {
         <Hero />
         <About />
         <Vision />
-        <Structure />
         <Programs />
         <Resources />
-        <Excellence />
         <Impact />
         <Team />
         <Contact />
