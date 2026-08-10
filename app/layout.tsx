7daec824bd353e339c062264556083611c6ab47c
ajyalna-admin <ajyalnaalwaidah@gmail.com>
2026-08-10 23:17:16 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { IBM_Plex_Sans_Arabic } from "next/font/google";
 import "./globals.css";
 import { brand } from "@/lib/data";
 import { FloatingSubscribe } from "@/components/FloatingSubscribe";
+import { WelcomeConfetti } from "@/components/WelcomeConfetti";
 
 const ibmPlexArabic = IBM_Plex_Sans_Arabic({
   subsets: ["arabic"],
@@ -36,6 +37,7 @@ export default function RootLayout({
         </div>
         {children}
         <FloatingSubscribe />
+        <WelcomeConfetti />
       </body>
     </html>
   );
